a913fc0d9848aaa78a6067ec45608966f1f03ecd
John Pierce <pierce.johndaniel@gmail.com>
2021-04-24 13:59:48 -0500

    feat: finished number 1 in exersice

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+    //======== using format ===============================================|
+        // double pi = 3.14159;
+        // System.out.format("The value of pi is approximately %f ", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+    //=============== #1 =========================================|
+            // System.out.print("Enter your favorite number!: ");
+                // int userInput = scanner.nextInt();
+
+    //=============== #2 ===========================================|
+            System.out.println("Enter three words: ");
+            String userInput = scanner.next();
+    }
+}
+
+
+//    String name = "codeup";
+//System.out.printf("Hello there, %s. Nice to see you.\n", name);
+
+
+
+
+//    String greeting = "Salutations";
+//    String name = "codeup";
+//System.out.printf("%s, %s!", greeting, name);
+
+
+
+
+//    Scanner scanner = new Scanner(System.in);
+//
+//System.out.print("Enter something: ");
+//        String userInput = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
