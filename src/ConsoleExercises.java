947c8a41c16e9241160099c09f55da03e8dbff63
John Pierce <pierce.johndaniel@gmail.com>
2021-04-24 16:41:27 -0500

    feat: finished # 2 3 and 4 and working on calculating perimeter and area of codeup class room

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -14,24 +14,45 @@ public class ConsoleExercises {
             // System.out.print("Enter your favorite number!: ");
                 // int userInput = scanner.nextInt();
 
-    //=============== #2 ===========================================|
-            System.out.println("Enter three words: ");
-            String userInput = scanner.next();
+    //=============== #2 =================================================================|
+        //  System.out.print("Enter three words: ");
+        //  String userInput = scanner.next();
+        //  String userSecondInput = scanner.next();
+        //  String userThirdInput = scanner.next();
+        //  System.out.printf("You entered:\n %s\n %s\n %s", userInput, userSecondInput,                 userThirdInput);
+
+    //=============== #3 and #4 ==========================================================|
+            // System.out.print("Enter you sentence:");
+            // String userSentence = scanner.next();
+            // String userSentence = scanner.nextLine();
+
+        //======== Experimenting with concatenation ================================|
+            // String userName = scanner.next();
+            // System.out.print("Your sentence said: " + userSentence + userName);
+
+        //=========== #4 ===================================================|
+            // System.out.printf("Your sentence said: %s ", userSentence);
+
+
+    //============ Calculate the perimeter and area of Codeup's classrooms =================|
+        //=========== #1 ============================
+            System.out.print("Enter the length of class room:");
+            String lengthAndWidth = scanner.nextLine();
+
+
+            System.out.printf("You Entered: %s ", lengthAndWidth);
     }
 }
 
+//=============================== Curriculum Notes =======================================>
 
 //    String name = "codeup";
 //System.out.printf("Hello there, %s. Nice to see you.\n", name);
 
 
-
-
 //    String greeting = "Salutations";
 //    String name = "codeup";
 //System.out.printf("%s, %s!", greeting, name);
-
-
 
 
 //    Scanner scanner = new Scanner(System.in);
