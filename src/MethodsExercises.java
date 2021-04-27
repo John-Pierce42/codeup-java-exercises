8ba27eddd91e8ebe79450e7def7c9e65f5ca1bc9
John Pierce <pierce.johndaniel@gmail.com>
2021-04-27 12:49:22 -0500

    feat: added medthods exercise class

---
+++ src/MethodsExercises.java
@@ -0,0 +1,33 @@
+public class MethodsExercises {
+
+    public static void main(String[] args){
+
+        addition(10,10);
+
+        System.out.println(subtraction(20,5));
+
+        multiplication(2, 5);
+
+        division(25,5);
+
+    }
+
+    public static void addition(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static void multiplication(int num1, int num2){
+        System.out.println(num1 * num2);
+
+    }
+
+    public static void division(int num1, int num2){
+        System.out.println(num1 / num2);
+
+    }
+
+}
