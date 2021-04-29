3bc37da99c7fc0cfc70f7ab0ff3feb4d4550661f
John Pierce <pierce.johndaniel@gmail.com>
2021-04-29 10:59:07 -0500

    feat: finished #5 of the high low game exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,10 +1,34 @@
-
+import java.util.Scanner;
 
 public class HighLow {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         int gamePicks = (int) (Math.random() * 100);
-        System.out.println(gamePicks);
+        System.out.println("The secret number is: " + gamePicks);
+        System.out.println();
+
+
+        System.out.println("Guess a number between 1 and 100.");
+        int userGuess;
+
+        do {
+
+            userGuess = scanner.nextInt();
+
+            if (userGuess < gamePicks) {
+                System.out.println("HIGHER.");
+
+            } else if (userGuess > gamePicks) {
+                System.out.println("LOWER.");
+
+            } else {
+                System.out.println("GOOD GUESS!");
+
+            }
+        } while (userGuess != gamePicks);
+
+
     }
 }
