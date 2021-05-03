2a4dbae97db9d77731d307950167270743f6a27e
John Pierce <pierce.johndaniel@gmail.com>
2021-05-03 12:51:20 -0500

    finished number validation class

--- src/util/Input.java
+++ src/util/Input.java
@@ -11,11 +11,46 @@ public class Input {
         return scanner.nextLine();
     }
 
-    boolean yesNo() {
-        if (getString().equalsIgnoreCase("y") || getString().equalsIgnoreCase("yes")) {
+    public boolean yesNo() {
+        if (getString().equals("y") || getString().equals("yes")) {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public int getInt() {
+        return scanner.nextInt();
+    }
+
+    public int getInt(int min, int max) {
+
+
+        int userInput = scanner.nextInt();
+        if (userInput > min && userInput < max) {
+            System.out.println("yes correct number");
+            return userInput;
+        } else {
+            System.out.println("enter another number.");
+            return getInt(min, max);
+        }
+
+
+    }
+
+    public double getDouble() {
+        return scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max) {
+
+        double userInput = scanner.nextInt();
+        if (userInput > min && userInput < max) {
+            System.out.println("yes correct number");
+            return userInput;
+        } else {
+            System.out.println("enter another number.");
+            return getDouble(min, max);
         }
     }
 
