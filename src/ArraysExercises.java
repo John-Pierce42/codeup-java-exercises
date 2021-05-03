55f6c65bb64736c3b98408eb1e40ea513b0394fc
John Pierce <pierce.johndaniel@gmail.com>
2021-05-03 17:03:37 -0500

    feat: working on persons array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static void main(String[] args){
+//============== toSring =============================|
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+//        System.out.println(Arrays.toString(numbers));
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
+// ================= Person array ========================|
+        Person[] personArray = new Person[3];
+        Person people1 = new Person("john");
+        Person people2 = new Person("gabby");
+        Person people3 = new Person("alexandra");
+        System.out.println(people[1]);
 
 
     }
--- src/Person.java
+++ src/Person.java
@@ -29,18 +29,18 @@ public class Person {
 
 
     public static void main(String[] args) {
-        Person john = new Person("John");
-//        john.name = "Johnny";
-        System.out.println(john.getName());
-        john.setName("Johnny");
-        System.out.println(john.getName());
-//        john.sayHello();
-
-
-        Person person1 = new Person("John");
-        Person person2 = new Person("John");
-        System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println(person1 == person2);
+//        Person john = new Person("John");
+////        john.name = "Johnny";
+//        System.out.println(john.getName());
+//        john.setName("Johnny");
+//        System.out.println(john.getName());
+////        john.sayHello();
+//
+//
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
@@ -53,6 +53,9 @@ public class Person {
 //        person2.setName("Jane");
 //        System.out.println(person1.getName());
 //        System.out.println(person2.getName());
+
+
+
 
 
 
