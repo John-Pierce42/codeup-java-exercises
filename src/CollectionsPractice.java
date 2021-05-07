4f0dd03ab6f2bbc60674fffc644c0d36d00ffbf5
John Pierce <pierce.johndaniel@gmail.com>
2021-05-07 11:44:56 -0500

    feat: added lecture

---
+++ src/CollectionsPractice.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class CollectionsPractice {
+
+
+//    ArrayList<String> names = new ArrayList<>(Arrays.asList(
+//                "Fred",
+//                "Sally",
+//                "Gabby"
+//
+//));
+
+//    public static ArrayList<String> upperLastElement(ArrayList<String> arr){
+//        if(arr.isEmpty()){
+//            return arr;
+//        } else if(arr.){
+//
+//        }
+//       return arr.;
+//    }
+//
+//    public static void main(String[] args){
+//
+////        upperLastElement().toUpperCase();
+//
+//
+//
+//
+//    }
+
+//=============================hash map ===============================================+|
+
+    //        Map<String, String> usernames = new HashMap<>();
+//        usernames.put("Ryan", "ryanorsinger");
+//        usernames.put("Luis", "MontealegreLuis");
+//        usernames.put("Zach", "zgulde");
+//        usernames.put("Fernando", "fmendozaro");
+//        usernames.put("Justin", "jreich5");
+//        System.out.println(usernames);
+
+//    Map<String, String> names = new HashMap<>();
+//    names.put("Pierce", "John");
+//
+}
