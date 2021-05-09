344e164d4669ff6d117dd5cbbbc08adb4cc166ef
John Pierce <pierce.johndaniel@gmail.com>
2021-05-09 12:41:47 -0500

    feat: finnished collections exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -43,32 +43,46 @@ public class GradesApplication {
         System.out.println("|" + students.get("John") + "|" + " " + "|" + students.get("Nic") + "|" + " " + "|" + students.get("Shan") + "|" + " " + "|" + students.get("Kenyan") + "|");
 
 
-
         System.out.println();
 
         System.out.println("What student would you like to see more information on?");
         String userInput = scanner.nextLine();
+        boolean userContinue = false;
 
-        if (userInput.equals("John42")) {
-            System.out.println("Name: John Pierce- GitHub Username: " + students.get("John"));
-            System.out.println("Current Average: " + John.getGradeAverage());
-        } else if (userInput.equals("Shan42")) {
-            System.out.println("Name: Shanshan Su - GitHub Username: " + students.get("Shan"));
-            System.out.println("Current Average: " + Shan.getGradeAverage());
-        } else if (userInput.equals("Nic42")) {
-            System.out.println("Name: Nicholas Martinez - GitHub Username: " + students.get("Nic"));
-            System.out.println("Current Average: " + Nic.getGradeAverage());
-        } else if (userInput.equals("Kenyan42")) {
-            System.out.println("Name: Kenyan Luce - GitHub Username: " + students.get("Kenyan"));
-            System.out.println("Current Average: " + Kenyan.getGradeAverage());
-        } else {
-            System.out.println("Sorry, no student found with the GitHub username of " + '"' + userInput + '"' + ".");
-        }
+        do {
 
-        if(userInput.equalsIgnoreCase("no")){
-            System.out.println("Goodbye, and have a wonderful day!");
-        }
+            if (userInput.equals("John42")) {
+                System.out.println("Name: John Pierce- GitHub Username: " + students.get("John"));
+                System.out.println("Current Average: " + John.getGradeAverage());
+            } else if (userInput.equals("Shan42")) {
+                System.out.println("Name: Shanshan Su - GitHub Username: " + students.get("Shan"));
+                System.out.println("Current Average: " + Shan.getGradeAverage());
+            } else if (userInput.equals("Nic42")) {
+                System.out.println("Name: Nicholas Martinez - GitHub Username: " + students.get("Nic"));
+                System.out.println("Current Average: " + Nic.getGradeAverage());
+            } else if (userInput.equals("Kenyan42")) {
+                System.out.println("Name: Kenyan Luce - GitHub Username: " + students.get("Kenyan"));
+                System.out.println("Current Average: " + Kenyan.getGradeAverage());
+            } else if (userInput.equalsIgnoreCase("no")) {
+                System.out.println("Goodbye, and have a wonderful day!");
+                break;
+            } else {
+                System.out.println("Sorry, no student found with the GitHub username of " + '"' + userInput + '"' + ".");
+            }
+
+            System.out.println("Would you like to see another student?");
+             userInput = scanner.nextLine();
+            if (userInput.equals("y")) {
+                System.out.println("What student would you like to see more information on?");
+                userInput = scanner.nextLine();
+                userContinue = true;
+            } else if (userInput.equalsIgnoreCase("no")) {
+                System.out.println("Goodbye, and have a wonderful day!");
+                userContinue = true;
+                break;
+            }
 
 
+        } while (userContinue);
     }
 }
