6c761d1953103a983221ab304b5b9c9ed9969c82
John Pierce <pierce.johndaniel@gmail.com>
2021-05-15 16:56:15 -0500

    feat: finished the exceptions and error handling exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,28 +7,33 @@ public class Input {
 
     private Scanner scanner;
 
-    public Input(){
+    public Input() {
         this.scanner = new Scanner(System.in);
     }
 
-    public  String getString() {
+    public String getString() {
         return scanner.nextLine();
     }
-
 
     public boolean yesNo() {
         String userInput = getString();
 
         if (userInput.equals("y") || userInput.equals("yes")) {
             return true;
-
         } else {
             return false;
         }
     }
 
     public int getInt() {
-        return scanner.nextInt();
+        String userInput;
+        userInput = this.getString();
+        try {
+            return Integer.valueOf(userInput);
+        } catch (NumberFormatException e){
+            System.out.println(userInput);
+            return getInt();
+        }
     }
 
     public int getInt(int min, int max) {
@@ -40,12 +45,17 @@ public class Input {
             System.out.println("enter another number.");
             return getInt(min, max);
         }
-
-
     }
 
     public double getDouble() {
-        return scanner.nextDouble();
+        String userInput;
+        userInput = this.getString();
+        try {
+            return Double.valueOf(userInput);
+        } catch (NumberFormatException e){
+            System.out.println(userInput);
+            return getDouble();
+        }
     }
 
     public double getDouble(double min, double max) {
@@ -58,5 +68,18 @@ public class Input {
             return getDouble(min, max);
         }
     }
+
+    public static void main(String[] args) {
+//    Input input = new Input();
+//        System.out.println("enter [y/n]");
+//        System.out.println(input.yesNo());
+
+//        System.out.println("enter a double number:");
+//        System.out.println(input.getDouble());
+//
+//        System.out.println("enter a int number:");
+//        System.out.println(input.getInt());
+//
+//    }
 
 }
